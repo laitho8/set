2ac3f29b06f26cdc9358b036abd9ba10f95b5a8c
ـــيث 𝕃𝕒𝕚𝕥𝕙 لـــ <127667772+laitho8@users.noreply.github.com>
2024-08-21 15:08:04 +0300

    Update IconIllustrationSafet.8fae630a.js

--- IconIllustrationSafet.8fae630a.js
+++ IconIllustrationSafet.8fae630a.js
@@ -1,1 +1,62 @@
 
+"use strict";
+(self.webpackChunk_twitter_responsive_web = self.webpackChunk_twitter_responsive_web || []).push([["shared~ondemand.SettingsInternals~bundle.LiveEvent~loader.TweetCurationActionMenu~icons/IconIllustrationSafet"], {
+    502359: (e,t,l)=>{
+        l.r(t),
+        l.d(t, {
+            default: ()=>s
+        });
+        var i = l(202784)
+          , r = l(208543)
+          , a = l(783427)
+          , c = l(473569);
+        const n = (e={})=>{
+            const {direction: t} = (0,
+            a.Z)();
+            return (0,
+            r.Z)("svg", {
+                ...e,
+                role: e["aria-label"] ? e.role || "img" : void 0,
+                "aria-hidden": void 0 === e["aria-label"],
+                style: [c.Z.root, e.style],
+                viewBox: "0 0 200 200",
+                children: i.createElement("g", null, i.createElement("circle", {
+                    cx: "100",
+                    cy: "100",
+                    fill: "#97E3FF",
+                    r: "100"
+                }), i.createElement("path", {
+                    d: "M57.575 121.5h29.408l25.142 28.858h16.367s6.425-12.867 6.425-50.133H52.042c0 15.817 5.533 21.275 5.533 21.275z",
+                    fill: "#005FD1"
+                }), i.createElement("path", {
+                    d: "M112.125 50.092L86.983 78.95H57.575s-5.533 5.458-5.533 21.275h82.875c0-37.267-6.425-50.133-6.425-50.133h-16.367z",
+                    fill: "#1DA1F2"
+                }), i.createElement("ellipse", {
+                    cx: "123.283",
+                    cy: "100.225",
+                    fill: "#97E3FF",
+                    rx: "6.567",
+                    ry: "45.075"
+                }), i.createElement("path", {
+                    d: "M147.408 146.8L48.225 59.867c-2.425-2.125-2.667-5.817-.542-8.242 2.125-2.425 5.817-2.667 8.242-.542l99.183 86.925c2.425 2.125 2.667 5.817.542 8.242-2.125 2.425-5.817 2.675-8.242.55z",
+                    fill: "#71C9F8"
+                }), i.createElement("path", {
+                    d: "M94.275 100.225h-7.058l30.808 27c-.233-2.108-.433-4.35-.608-6.717l-23.142-20.283zm33.942 29.75c-.25 1.983-.533 3.808-.833 5.458l4.325 3.792c.324-1.592.65-3.375.957-5.35l-4.45-3.9zm-33.942-29.75L70 78.958h-7.058l24.275 21.267",
+                    fill: "#005FD1"
+                }), i.createElement("path", {
+                    d: "M118.025 127.225l9.358 8.208c.3-1.65.575-3.475.833-5.458l-10.8-9.467c.176 2.367.376 4.617.61 6.717z",
+                    fill: "#1DA1F2"
+                }))
+            }, {
+                writingDirection: t
+            })
+        }
+        ;
+        n.metadata = {
+            width: 200,
+            height: 200
+        };
+        const s = n
+    }
+}]);
+//# sourceMappingURL=https://ton.local.twitter.com/responsive-web-internal/sourcemaps/client-web/shared~ondemand.SettingsInternals~bundle.LiveEvent~loader.TweetCurationActionMenu~icons/IconIllustrationSafet.8fae630a.js.map
